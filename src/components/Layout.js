39c0b387205325be49e3131f30477276b9994acc
Adri Ana <szopiangreg@gmail.com>
2021-03-30 00:02:25 +0100

    add layout

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -3,8 +3,25 @@ import "./Layout.css";
 
 const Layout = (props) => {
   return (
-    <div>
-      <h1>hi you</h1>
+    <div className="frame">
+      <div className="calculator">
+        <br></br>
+        <img />
+        <div className="keys">
+          <input
+            type="button"
+            value={"C"}
+            className="button clear"
+            onClick={() => {}}
+          ></input>
+          <input
+            type="button"
+            value={"DEL"}
+            className="button clear"
+            onClick={() => {}}
+          ></input>
+        </div>
+      </div>
     </div>
   );
 };
